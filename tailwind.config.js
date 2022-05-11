70c7c46bf9538286eaca9416ce3166a1ecbfa19c
vincejs77 <vinjobs77@gmail.com>
2022-05-12 01:52:35 +0200

    first commit 001

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	// npx tailwindcss -i ./src/assets/css/inputs_styles.css -o ./public/css/outputs_styles.css --minify --watch
+	content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+	theme: {
+		extend: {},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			white: "#ffffff",
+			cblack: "#002037",
+			cgray: {
+				100: "#60606013",
+				800: "#60606097",
+				900: "#606060"
+			},
+			cgreen: "#009C98",
+			cyellow: "#FFCE00"
+		}
+	},
+	plugins: []
+};
